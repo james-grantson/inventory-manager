7144be81dbe4ee0ce7774fd9eceda8704bab239e
james-grantson <grantsonjames9@gmail.com>
2026-03-02 15:16:43 -0800

    Fix typo: setGreating -> setGreeting

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function DashboardPage() {
 
   useEffect(() => {
     const hour = new Date().getHours()
-    if (hour < 12) setGreating('Good Morning')
+    if (hour < 12) setGreeting('Good Morning')
     else if (hour < 18) setGreeting('Good Afternoon')
     else setGreeting('Good Evening')
     
