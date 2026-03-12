df6d9c378c55b840b793655cb8c7e68f95c4a489
james-grantson <grantsonjames9@gmail.com>
2026-03-12 05:29:48 -0700

    Remove organizationName prop from PDFReportGenerator (fix type error)

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -140,7 +140,7 @@ export default function ReportsPage() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
           >
-            <PDFReportGenerator products={products} organizationName={currentOrganization?.name} />
+            <PDFReportGenerator products={products} />
           </motion.div>
 
           <motion.div
